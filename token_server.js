c21e6d49be21e7fe73c8b410d932bca3abcbde5a
jeslin <jeslin@qburst.com>
2015-09-09 13:14:14 +0530

    port changed to 9500

--- token_server.js
+++ token_server.js
@@ -43,7 +43,7 @@ req.on('data', function(chunk) {
 })
 
 
-var server = app.listen(9001, function () {
+var server = app.listen(9500, function () {
 
   var host = server.address().address
   var port = server.address().port
